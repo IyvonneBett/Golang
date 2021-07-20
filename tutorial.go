fdce9afde85a91c06662cd2407b1d3e05896ee36
IyvonneBett <yvonnebett19@students.uonbi.ac.ke>
2021-07-20 23:55:04 +0300

    added files

---
+++ tutorial.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	//"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type the year you were born: ")
+	scanner.Scan()
+
+	input := scanner.Text()
+	fmt.Printf("You typed: %q ", input)
+}
